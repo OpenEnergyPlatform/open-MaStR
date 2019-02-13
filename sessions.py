1488b8a02f587b0767f0661ba03204dd5cc04752
Jonas Hoersch <jonas@chaoflow.net>
2019-02-13 11:59:30 +0100

    Introduce a dedicated logging filter to hide time parsing exceptions

--- sessions.py
+++ sessions.py
@@ -153,9 +153,48 @@ def mastr_session():
 
     wsdl = 'https://www.marktstammdatenregister.de/MaStRAPI/wsdl/mastr.wsdl'
     transport = Transport(cache=SqliteCache())
-    settings = Settings(strict=False, xml_huge_tree=True)
+    settings = Settings(strict=True, xml_huge_tree=True)
     client = Client(wsdl=wsdl, transport=transport, settings=settings)
     client_bind = client.bind('Marktstammdatenregister', 'Anlage')
 
+    mastr_suppress_parsing_errors(['parse-time-second'])
+
     print('MaStR API connection established.')
     return client, client_bind, token, user
+
+def mastr_suppress_parsing_errors(which_errors):
+    """
+    Install logging filters into zeep type parsing modules to suppress
+
+    Arguments
+    ---------
+    which_errors : [str]
+        Names of errors defined in `error_filters` to set up.
+        Currently one of ('parse-time-second').
+
+    NOTE
+    ----
+    zeep and mastr don't seem to agree on the correct time format. Instead of
+    suppressing the error, we should fix the parsing error, or they should :).
+    """
+
+    class FilterExceptions(logging.Filter):
+        def __init__(self, name, klass, msg):
+            super().__init__(name)
+
+            self.klass = klass
+            self.msg = msg
+
+        def filter(self, record):
+            if record.exc_info is None: return 1
+
+            kl, inst, tb = record.exc_info
+            return 0 if isinstance(inst, self.klass) and inst.args[0] == self.msg else 1
+
+    # Definition of available filters
+    error_filters = [FilterExceptions('parse-time-second', ValueError, 'second must be in 0..59')]
+
+    # Install filters selected by `which_errors`
+    zplogger = logging.getLogger('zeep.xsd.types.simple')
+    zplogger.filters = ([f for f in zplogger.filters if not isinstance(f, FilterExceptions)] +
+                        [f for f error_filters if f.name in which_errors])
