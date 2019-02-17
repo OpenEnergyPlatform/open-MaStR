6f50a1cacc23ec1d24235b405f1d7f2e6e7f9f37
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2019-02-17 05:04:57 +0100

    update metadata

--- mastr_power_unit_download.py
+++ mastr_power_unit_download.py
@@ -62,7 +62,7 @@ def get_power_unit(start_from, limit=2000):
     return power_unit
 
 
-def download_power_unit(power_unit_list_len=1847110, limit=2000):
+def download_power_unit(power_unit_list_len=1848000, limit=2000):
     """Download StromErzeuger.
 
     Arguments
@@ -75,7 +75,7 @@ def download_power_unit(power_unit_list_len=1847110, limit=2000):
     Existing units:
     1822000 (2019-02-10)
     1844882 (2019-02-15)
-    1847110 (2019-02-17)
+    1847117 (2019-02-17)
     """
 
     data_version = get_data_version()
