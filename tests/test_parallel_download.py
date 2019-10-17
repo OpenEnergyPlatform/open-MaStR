bec6f228836335ef32f9ad38d8e25073e936ef6f
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2019-10-17 15:07:07 +0200

    Add test for power_unit_download

--- tests/test_parallel_download.py
+++ tests/test_parallel_download.py
@@ -6,10 +6,18 @@ __version__ = "v0.8.0"
 
 
 import pandas as pd
+import os
 
 from soap_api.mastr_power_unit_download import (
     get_power_unit,
+    download_power_unit,
 )
+
+TEST_DATA = 'tests/data/bnetza_mastr_test_power-unit.csv'
+
+def remove_test_data(fname=TEST_DATA):
+    os.remove(fname)
+    os.rmdir(os.path.dirname(fname))
 
 
 def test_get_power_unit_return_data_frame():
@@ -21,3 +29,17 @@ def test_get_power_unit_return_correct_number():
     n = 13
     power_units = get_power_unit(0, n)
     assert len(power_units.index) == n
+
+
+def test_download_power_unit_unique_mastr_number():
+    n = 100
+    # download the data
+    download_power_unit(n, ofname=TEST_DATA)
+    # load the data
+    df = pd.read_csv(TEST_DATA, sep=';')
+    # remove the data
+    remove_test_data()
+    assert len(df.EinheitMastrNummer.unique()) == n
+    assert len(df.EegMastrNummer.unique()) == n
+
+
