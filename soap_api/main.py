cff4916946574084feecbe5a0da17b203c4f6c4e
Ludee <Ludwig.Huelk@rl-institut.de>
2019-12-01 17:04:22 +0100

    restart main.py to finish data version 2.1.2 #54

--- soap_api/main.py
+++ soap_api/main.py
@@ -59,19 +59,19 @@ if __name__ == "__main__":
 
     """Wind"""
 #    download_unit_wind()
-    download_unit_wind_eeg()
+    # download_unit_wind_eeg()
     # download_unit_wind_permit()
     # make_wind()
-    #
-    # """Hydro"""
-    # download_unit_hydro()
-    # download_unit_hydro_eeg()
-    # make_hydro()
-    #
-    # """Biomass"""
-    # download_unit_biomass()
-    # download_unit_biomass_eeg()
-    # make_biomass()
+
+    """Hydro"""
+    download_unit_hydro()
+    download_unit_hydro_eeg()
+    make_hydro()
+
+    """Biomass"""
+    download_unit_biomass()
+    download_unit_biomass_eeg()
+    make_biomass()
     #
     # """Solar"""
     # ''' DEFAULT PARAMS: start_from=0, n_entries=1, parallelism=300, cpu_factor=1, overwrite=False '''
