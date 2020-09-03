2d220d300a83348904802b6530eefc22cc9acc18
jadeaffenjaeger <elias.trommer@rl-institut.de>
2020-09-03 09:50:43 +0200

    Separate control logic from parallel function
    This moves the control code for the parallel execution to a separate
    function in order to make it more extendable and maintain better
    readability in the parallelization function.

--- soap_api/parallel.py
+++ soap_api/parallel.py
@@ -6,6 +6,24 @@ from soap_api.utils import write_to_csv
 from soap_api.utils import is_time_blacklisted
 log = logging.getLogger(__name__)
 
+last_successful_download = datetime.datetime.now()
+
+# Check if worker threads need to be killed
+def _stop_execution(time_blacklist, timeout):
+    # Last successful execution was more than 10 minutes ago. Server seems unresponsive, so stop execution permanently by raising an error
+    if last_successful_download + datetime.timedelta(minutes=timeout) < datetime.datetime.now():
+        log.error('No response from server in the last {} minutes. Stopping execution.'.format(timeout))
+        raise ConnectionAbortedError
+    # Stop execution smoothly if current system time is in blacklist by returning. Calling function can decide to continue running later if needed
+    if time_blacklist and is_time_blacklisted(now.time()):
+        log.info('Current time is in blacklist. Halting.')
+        return True
+    # ... Add more checks here if needed ...
+    return False
+
+def _reset_timeout():
+    last_successful_download = datetime.datetime.now()
+
 def parallel_download(unit_list, func, filename, threads=4, timeout=10, time_blacklist=True):
     """Download a list of units using a pool of threads
 
@@ -30,21 +48,15 @@ def parallel_download(unit_list, func, filename, threads=4, timeout=10, time_bla
         exit as soon as current time is blacklisted
     """
 
+    _reset_timeout()
     with multiprocessing.Pool(threads) as pool:
-        last_successful = datetime.datetime.now()
         for unit in pool.imap_unordered(func, unit_list):
-            now = datetime.datetime.now()
             # Check if data was retrieved successfully
             if unit is not None:
-                last_successful = now
-                # TODO Check if low-level access can be done for all subfunctions
+                _reset_timeout()
+                # TODO Check if low-level access can be done for all potential subfunctions
+                # Alternatively turn this into a status bar
                 log.info('Unit {} sucessfully retrieved.'.format(unit.loc[1, 'EinheitMastrNummer']))
                 write_to_csv(filename, unit)
-            # Last successful execution was more than 10 minutes ago, so stop execution
-            if last_successful + datetime.timedelta(minutes=timeout) < datetime.datetime.now():
-                log.error('No response from server in the last {} minutes. Stopping execution.'.format(timeout))
-                raise ConnectionAbortedError
-            # Stop execution if current system time is in blacklist
-            if time_blacklist and is_time_blacklisted(now.time()):
-                log.info('Current time is in blacklist. Halting.')
+            if _stop_execution(time_blacklist, timeout) is True:
                 break
