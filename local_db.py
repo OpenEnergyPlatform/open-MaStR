4b720990c575fa20957789dbb47e52c539014b17
Guido Pleßmann <guido.plessmann@rl-institut.de>
2020-10-10 14:24:21 +0200

    Import all MaStR raw data
    * Move geom creation to prio DB import (do it in Python)
    * Fix geometry column definition such that it's properly recognized by
    PostGIS
    * Consider all datasets relevant in cleansing SQL scripts

--- local_db.py
+++ local_db.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from geoalchemy2 import Geometry
+from geoalchemy2 import Geometry, WKTElement
 import os
 from urllib.request import urlretrieve
 from soap_api.utils import DATA_VERSION
 import soap_api.utils as soap_utils
+import geopandas as gpd
+from shapely.wkb import loads as wkb_loads
 
 
 BKG_VG250 = {
@@ -24,7 +26,7 @@ DATA_BASE_PATH = "data"
 MASTR_RAW_SCHEMA = "model_draft"
 OPEN_MASTR_SCHEMA = "model_draft"
 
-TECHNOLOGIES = ["nuclear"] #["combustion", "wind", "hydro", "solar", "biomass", "gsgk", "storage", "nuclear"]
+TECHNOLOGIES = ["nuclear", "wind", "hydro", "solar", "biomass", "combustion"] #, "gsgk", "storage"]
 
 
 def engine_local_db():
@@ -36,7 +38,7 @@ ENGINE_LOCAL = engine_local_db()
 
 def table_name_from_file(filename):
 
-    table_name = filename.replace(DATA_BASE_PATH + "/", "").replace("_" + DATA_VERSION, "").replace(".csv", "")
+    table_name = filename.replace(DATA_BASE_PATH + "/", "").replace("_" + DATA_VERSION, "").replace(".csv", "") + "_clean"
     return table_name
 
 
@@ -58,21 +60,16 @@ def wkb_hexer(line):
     return line.wkb_hex
 
 
-def read_csv_to_db(csv_file, table, schema, index_col, conn, geom_col="geom", sep=","):
+def table_to_db(csv_data, table, schema, conn, geom_col="geom"):
     # Create schemas
     query = "CREATE SCHEMA IF NOT EXISTS {schema}".format(schema=schema)
     conn.execute(query)
 
-    # CSV file
-    csv_data = pd.read_csv(csv_file,
-                           index_col=index_col,
-                           sep=sep)
-
-    # Insert data into database
     csv_data.to_sql(table,
                     con=conn,
                     schema=schema,
-                    dtype={geom_col: Geometry()},
+                    # dtype={geom_col: Geometry("POINT", srid=4326)},
+                    dtype={geom_col: Geometry(srid=4326)},
                     if_exists="replace")
 
 
@@ -97,10 +94,17 @@ def import_boundary_data_csv(schema, table, index_col="id"):
                     OEP_QUERY_PATTERN.format(schema=schema, table=table),
                     csv_file)
             else:
-                print("CSV file exists")
+                print("CSV file {} exists".format(csv_file))
 
-            # Read CSV and insert to db
-            read_csv_to_db(csv_file.format(schema=schema, table=table), table, schema, index_col, con)
+            # Read CSV file
+            csv_data = pd.read_csv(csv_file,
+                                   index_col=index_col)
+
+            # Prepare geom data for DB upload
+            csv_data["geom"] = csv_data["geom"].apply(lambda x: WKTElement(wkb_loads(x, hex=True).wkt, srid=4326))
+
+            # Insert to db
+            table_to_db(csv_data, table, schema, con)
         else:
             print("Table '{schema}.{table}' already exists in local database".format(schema=schema, table=table))
 
@@ -110,12 +114,36 @@ def import_bnetz_mastr_csv():
 
     with ENGINE_LOCAL.connect() as con:
         for k, d in csv_db_mapping.items():
-            read_csv_to_db("soap_api/" + d["file"],
-                           d["table"],
-                           MASTR_RAW_SCHEMA,
-                           "lid",
-                           con,
-                           sep=";")
+            # Read CSV file
+            csv_data = pd.read_csv("soap_api/" + d["file"].replace("data", "data/saved"),
+                                   index_col="lid",
+                                   sep=";")
+
+            # Create 'geom' column from lat/lon
+            gdf = add_geom_col(csv_data)
+
+            # Import to local database
+            table_to_db(gdf,
+                        d["table"],
+                        MASTR_RAW_SCHEMA,
+                        con,
+                        geom_col="geom")
+
+
+def add_geom_col(df, lat_col="Breitengrad", lon_col="Laengengrad", srid=4326):
+
+    df_with_coords = df.loc[~(df["Breitengrad"].isna() | df["Laengengrad"].isna())]
+    df_no_coords = df.loc[(df["Breitengrad"].isna() | df["Laengengrad"].isna())]
+
+    gdf = gpd.GeoDataFrame(
+        df_with_coords, geometry=gpd.points_from_xy(df_with_coords[lon_col], df_with_coords[lat_col]),
+        crs="EPSG:{}".format(srid))
+    gdf["geom"] = gdf["geometry"].apply(lambda x: WKTElement(x.wkt, srid=srid))
+    gdf.drop(columns=["geometry"], inplace=True)
+
+    gdf = gdf.append(df_no_coords)
+
+    return gdf
 
 
 if __name__ == "__main__":
@@ -127,3 +155,5 @@ if __name__ == "__main__":
 
     # TODO: replace prints with logger
     # TODO: make entire workflow also work on OEP (for Ludwig)
+    # TODO: grant to oeuser: make this optional
+    # TODO: Sort functions, rename, find right location in repository
