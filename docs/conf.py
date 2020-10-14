b640f2c939ea1b2fe6c872c2fd1297dd69d4b1b5
Guido Pleßmann <guido.plessmann@rl-institut.de>
2020-10-14 21:58:46 +0200

    Update API docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../soap_api'))
+sys.path.insert(0, os.path.abspath('../open_mastr'))
 
 
 # -- Project information -----------------------------------------------------
