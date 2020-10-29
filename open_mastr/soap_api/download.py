7b24a38b1b201e60de41e86d3ee2e2976790feec
Guido Pleßmann <guido.plessmann@rl-institut.de>
2020-10-29 21:00:34 +0100

    Provide basic to_csv function

--- open_mastr/soap_api/download.py
+++ open_mastr/soap_api/download.py
@@ -6,9 +6,12 @@ from zeep.transports import Transport
 import requests
 import logging
 import pandas as pd
-from zeep.exceptions import XMLParseError
+import time
+from zeep.exceptions import XMLParseError, Fault
+import os
 
 from open_mastr.utils import credentials as cred
+from open_mastr.soap_api.config import get_filenames, get_project_home_dir, get_data_config
 
 
 log = logging.getLogger(__name__)
@@ -383,11 +386,20 @@ def _unit_data(mastr_api, energy_carrier, unit_mastr_id=[], eeg=True, limit=None
     kwk_data = _flatten_dict(kwk_data)
     permit_data = _flatten_dict(permit_data)
 
-    # merge data
-    unit_data_df = pd.DataFrame(unit_data).set_index("EinheitMastrNummer")
+    # Join data to a single dataframe
+    idx_cols = [(units, "EinheitMastrNummer", ""),
+                (unit_data, "EinheitMastrNummer", "_unit"),
+                (eeg_data, "VerknuepfteEinheit", "_eeg"),
+                (kwk_data, "VerknuepfteEinheiten", "_kwk"),
+                (permit_data, "VerknuepfteEinheiten", "_permit")]
 
-    # return unit_data
-    return units, unit_data, eeg_data, kwk_data, permit_data
+    joined_data = pd.DataFrame(idx_cols[0][0]).set_index(idx_cols[0][1])
+
+    for dat, idx_col, suf in idx_cols[1:]:
+        if dat:
+            joined_data = joined_data.join(pd.DataFrame(dat).set_index(idx_col), rsuffix=suf)
+
+    to_csv(joined_data, energy_carrier)
 
 
 def _retrieve_additional_unit_data(units, energy_carrier, mastr_api):
@@ -540,6 +552,16 @@ def _flatten_dict(data):
                 dic.update({k: dic[k]["Wert"]})
 
     return data
+
+
+def to_csv(df, technology):
+    DATA_VERSION = get_data_config()["data_version"]
+    DATA_PATH = os.path.join(get_project_home_dir(), "data", DATA_VERSION)
+    filenames = get_filenames()
+
+    csv_file = os.path.join(DATA_PATH, filenames["raw"][technology]["joined"])
+
+    df.to_csv(csv_file, index=True, index_label="EinheitMastrNummer", encoding='utf-8')
 
 
 class _MaStRDownloadFactory(type):
