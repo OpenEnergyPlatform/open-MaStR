5066dd46b7441a04aae5f4d28a73d2f47c03e2b3
Florian Kotthoff <florian.kotthoff@posteo.de>
2021-12-07 10:20:02 +0100

    Add function annotation

--- open_mastr/xml_parser/utils_sqlite_bulk_cleansing.py
+++ open_mastr/xml_parser/utils_sqlite_bulk_cleansing.py
@@ -1,5 +1,6 @@
 import pathlib
 from collections import ChainMap
+import sqlite3
 from typing import List, Dict, Tuple
 from defusedxml.ElementTree import parse
 from zipfile import ZipFile
@@ -9,39 +10,45 @@ import re
 
 
 def cleansing_sqlite_database_from_bulkdownload(
-    con, zipped_xml_file_path, xml_folder_path
-):
+    con: sqlite3.Connection, zipped_xml_file_path: str, xml_folder_path: str
+) -> None:
     """The cleansing of the bulk download data consists of the following parts:
     - replace the katalogeintraege
     """
     replace_mastr_katalogeintraege(con, zipped_xml_file_path, xml_folder_path)
 
 
-def replace_mastr_katalogeintraege(con, zipped_xml_file_path, xml_folder_path):
+def replace_mastr_katalogeintraege(
+    con: sqlite3.Connection, zipped_xml_file_path: str, xml_folder_path: str
+) -> None:
     catalog = make_catalog_from_mastr_xml_files(zipped_xml_file_path, xml_folder_path)
     execute_message = "SELECT name FROM sqlite_master WHERE TYPE='table';"
     cursor = con.cursor()
     cursor.execute(execute_message)
     tables_list = cursor.fetchall()
-    pattern = re.compile(r'cleansed|katalog')
+    pattern = re.compile(r"cleansed|katalog")
     for table_name_tuple in tables_list:
         table_name = table_name_tuple[0]
-        if not re.search(pattern,table_name):
+        if not re.search(pattern, table_name):
             replace_katalogeintraege_in_single_table(con, table_name, catalog)
 
 
-def replace_katalogeintraege_in_single_table(con, table_name, catalog):
+def replace_katalogeintraege_in_single_table(
+    con: sqlite3.Connection, table_name: str, catalog: dict
+) -> None:
     """This still seems to have errors."""
     df = pd.read_sql(f"SELECT * FROM {table_name};", con)
     for column_name in df.columns:
         if column_name in catalog.keys():
             df[column_name] = df[column_name].astype("Int64").map(catalog[column_name])
-    cleansed_table_name=table_name + "_cleansed"
-    df.to_sql(cleansed_table_name,con,index=False,if_exists='replace')
+    cleansed_table_name = table_name + "_cleansed"
+    df.to_sql(cleansed_table_name, con, index=False, if_exists="replace")
     print(f"Data in table {table_name} was sucessfully cleansed.")
 
 
-def make_catalog_from_mastr_xml_files(zipped_xml_file_path, xml_folder_path):
+def make_catalog_from_mastr_xml_files(
+    zipped_xml_file_path: str, xml_folder_path: str
+) -> dict:
     CATALOG_MAPPING = {
         "NACEGruppeAbschnitt": "HauptwirtdschaftszweigAbschnitt",
         "NACEGruppeAbteilung": "HauptwirtdschaftszweigAbteilung",
@@ -64,14 +71,14 @@ def make_catalog_from_mastr_xml_files(zipped_xml_file_path, xml_folder_path):
     return catalog
 
 
-def get_categories(zipped_xml_file_path, xml_folder_path):
+def get_categories(zipped_xml_file_path: str, xml_folder_path: str) -> Tuple[str, int]:
     with ZipFile(zipped_xml_file_path, "r") as f:
         for file_name in f.namelist():
             if file_name == "Katalogkategorien.xml":
                 categories_extracted_filepath = f.extract(
                     file_name, path=xml_folder_path
                 )
-                
+
                 if not categories_extracted_filepath:
                     raise FileNotFoundError(
                         f"Kann die Datei '{file_name}' aus dem MaStR-Datensatz nicht finden."
@@ -85,13 +92,13 @@ def get_categories(zipped_xml_file_path, xml_folder_path):
                     yield attributes["Name"], attributes["Id"]
 
 
-def get_values_for_category(category_id, zipped_xml_file_path,xml_folder_path):
+def get_values_for_category(
+    category_id: int, zipped_xml_file_path: str, xml_folder_path: str
+) -> dict:
     with ZipFile(zipped_xml_file_path, "r") as f:
         for file_name in f.namelist():
             if file_name == "Katalogwerte.xml":
-                values_extracted_filepath = f.extract(
-                    file_name, path=xml_folder_path
-                )
+                values_extracted_filepath = f.extract(file_name, path=xml_folder_path)
     if not values_extracted_filepath:
         raise FileNotFoundError(
             f"Kann die Datei '{file_name}' aus dem MaStR-Datensatz nicht finden."
