ed2052dfbee4d5f19189750e9b7a53a2109eaf84
Florian Kotthoff <florian.kotthoff@posteo.de>
2021-12-09 14:15:19 +0100

    Create test functions

---
+++ tests/test_utils_download_bulk.py
@@ -0,0 +1,16 @@
+import tempfile
+import os
+from open_mastr.xml_parser.utils_download_bulk import download_xml_Mastr, get_url_from_Mastr_website
+
+def test_get_url_from_Mastr_website():
+    url = get_url_from_Mastr_website
+    assert "marktstammdatenregister" in url
+
+def test_download_xml_Mastr():
+    url = "https://jsonplaceholder.typicode.com/todos/1"
+    save_path = os.path.join(tempfile.gettempdir(),"tempjson.txt") 
+    download_xml_Mastr(url=url, save_path=save_path)
+    with open(save_path, "r") as f:
+        data = f.read()
+        assert len(data) > 10
+
