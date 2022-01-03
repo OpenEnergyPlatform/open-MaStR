9bc4821b3526b8c0dba281ac4c51e52d6bcc7efa
Florian Kotthoff <florian.kotthoff@posteo.de>
2022-01-03 13:19:15 +0100

    Update test functions for xml download

---
+++ tests/xml_download/test_utils_download_bulk.py
@@ -0,0 +1,23 @@
+import tempfile
+import os
+
+from open_mastr.xml_download.utils_download_bulk import (
+    get_url_from_Mastr_website,
+    download_xml_Mastr,
+)
+
+
+def test_get_url_from_Mastr_website():
+    url = get_url_from_Mastr_website()
+    assert len(url) > 10
+    assert type(url) == str
+    assert "marktstammdaten" in url
+
+
+def test_download_xml_Mastr():
+    url = "https://jsonplaceholder.typicode.com/todos/1"
+    save_path = os.path.join(tempfile.gettempdir(), "tempjson.txt")
+    download_xml_Mastr(url=url, save_path=save_path)
+    with open(save_path, "r") as f:
+        data = f.read()
+        assert len(data) > 10
--- tests/xml_download/test_utils_sqlite_bulk_cleansing.py
+++ tests/xml_download/test_utils_sqlite_bulk_cleansing.py
@@ -1,10 +1,11 @@
 from open_mastr.xml_download.utils_sqlite_bulk_cleansing import (
     create_katalogwerte_from_sqlite,
+    replace_katalogeintraege_in_single_table,
 )
 import sqlite3
-from open_mastr.mastr import Mastr
 from os.path import expanduser
 import os
+from open_mastr.xml_download.colums_to_replace import columns_replace_list
 
 
 def test_create_katalogwerte_from_sqlite():
@@ -14,3 +15,28 @@ def test_create_katalogwerte_from_sqlite():
     assert type(katalogwerte) == dict
     assert len(katalogwerte) > 1000
     assert type(list(katalogwerte.keys())[0]) == int
+
+
+def test_replace_katalogeintraege_in_single_table():
+    _sqlite_folder_path = os.path.join(expanduser("~"), ".open-MaStR", "data", "sqlite")
+    con = sqlite3.connect(os.path.join(_sqlite_folder_path, "bulksqlite.db"))
+    table_name = "einheitenwasser"
+    katalogwerte = create_katalogwerte_from_sqlite(con)
+
+    replace_katalogeintraege_in_single_table(
+        con=con,
+        table_name=table_name,
+        katalogwerte=katalogwerte,
+        columns_replace_list=columns_replace_list,
+    )
+
+    new_table_name = table_name + "_cleansed"
+
+    c = con.cursor()
+    query = f"SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{table_name}'"
+    c.execute(query)
+
+    assert c.fetchone()[0] == 1
+
+    c.close()
+    con.close()
