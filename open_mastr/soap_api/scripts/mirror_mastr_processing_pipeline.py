226fc9d36297492a77b72a8aa378e12ae9d08494
cwm <djeenie@posteo.de>
2022-01-19 09:23:46 +0000

    Add mirror_mastr_processing_pipeline.py

---
+++ open_mastr/soap_api/scripts/mirror_mastr_processing_pipeline.py
@@ -0,0 +1,10 @@
+from open_mastr.data_io import zenodo_upload, read_csv_data
+from open_mastr.postprocessing.cleaning import cleaned_data
+from open_mastr.postprocessing.postprocessing import postprocess
+from open_mastr.postprocessing.postprocessing import to_csv
+
+cleaned = cleaned_data()
+zenodo_upload(data_stages=["raw", "cleaned"])
+cleaned = read_csv_data("cleaned")
+postprocess(cleaned)
+to_csv()
