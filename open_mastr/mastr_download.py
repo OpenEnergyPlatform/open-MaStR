56ee16a6f349dfba35471b236bb57c30c816308d
cwm <djeenie@posteo.de>
2022-01-25 13:06:38 +0000

    Adapt to new structure of specifying DB type

--- open_mastr/mastr_download.py
+++ open_mastr/mastr_download.py
@@ -1,4 +1,5 @@
 from open_mastr.mastr import Mastr
+from open_mastr.settings import DB_ENGINE
 
 # instantiate Mastr() class
 mastr = Mastr()
@@ -8,11 +9,10 @@ date='latest'
 limit = None
 processes = 12
 chunksize = 1000
-initialize_db='sqllite'
 technology = ["wind", "biomass", "combustion", "gsgk", "hydro", "nuclear", "storage", "solar"]
 data_types = ["unit_data", "eeg_data", "kwk_data", "permit_data"]
 location_types = ["location_elec_generation", "location_elec_consumption", "location_gas_generation",
                   "location_gas_consumption"]
 
 # call download functionality
-mastr.download(method='API', processes=processes, limit=limit, chunksize=chunksize, technology=technology, date=date, data_types=data_types, location_types=location_types, initialize_db=initialize_db)
+mastr.download(method='API', processes=processes, limit=limit, chunksize=chunksize, technology=technology, date=date, data_types=data_types, location_types=location_types, initialize_db=DB_ENGINE)
