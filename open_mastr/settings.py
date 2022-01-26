751d517d90d030e5257a54cc767ef11f57b5d3c5
Florian Kotthoff <florian.kotthoff@posteo.de>
2022-01-26 12:59:30 +0100

    Add TODO

--- open_mastr/settings.py
+++ open_mastr/settings.py
@@ -1,5 +1,6 @@
 import os
 
+# TODO: Migrate the DB settings from the bulk download
 DB_ENGINE = os.environ.get("DB_ENGINE", "sqlite")
 DB_URL = (
     "postgresql+psycopg2://open-mastr:open-mastr@localhost:55443/open-mastr"
