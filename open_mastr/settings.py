c464c6f25a8843a0648ff028d397f85949ba16c5
Florian Kotthoff <florian.kotthoff@posteo.de>
2022-02-02 12:21:56 +0100

    Set sqlite database path

--- open_mastr/settings.py
+++ open_mastr/settings.py
@@ -1,10 +1,15 @@
 import os
+from os.path import expanduser
 
 # TODO: Migrate the DB settings from the bulk download
 DB_ENGINE = os.environ.get("DB_ENGINE", "sqlite")
 
+sqlite_database_path = os.path.join(
+            expanduser("~"), ".open-MaStR", "data", "sqlite", "apisqlite.db"
+        )
+
 print(DB_ENGINE)
 DB_URL = (
     "postgresql+psycopg2://open-mastr:open-mastr@localhost:55443/open-mastr"
-    if DB_ENGINE == "docker" else "sqlite:///master.db"
+    if DB_ENGINE == "docker" else f"sqlite:///{sqlite_database_path}"
 )
