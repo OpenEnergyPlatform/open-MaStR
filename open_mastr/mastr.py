d701a00057b02d2d8ee68a956d11dc967159e3b8
Florian Kotthoff <florian.kotthoff@posteo.de>
2022-02-02 14:14:26 +0100

    Correct code to match flake8

--- open_mastr/mastr.py
+++ open_mastr/mastr.py
@@ -10,7 +10,6 @@ import numpy as np
 # import xml dependencies
 from os.path import expanduser
 from open_mastr.xml_download.utils_download_bulk import (
-    get_url_from_Mastr_website,
     download_xml_Mastr,
 )
 from open_mastr.xml_download.utils_write_sqlite import convert_mastr_xml_to_sqlite
@@ -23,23 +22,11 @@ from open_mastr.soap_api.mirror import MaStRMirror
 
 
 class Mastr:
-    def __init__(self, date_string="today") -> None:
+    def __init__(self) -> None:
         # TODO: Should parameter date_string be defined here or later in the download function?
-        if date_string == "today":
-            self._today_string = date.today().strftime("%Y%m%d")
-        else:
-            try:
-                self._today_string = isoparse(date_string).strftime("%Y%m%d")
-            except dateutil.parser.ParserError:
-                print("date_string has to be a proper date in the format yyyymmdd.")
-                raise
-        # Define the paths
+        # Define the paths for the zipped xml download and the sql databases
         self._xml_folder_path = os.path.join(
             expanduser("~"), ".open-MaStR", "data", "xml_download"
-        )
-
-        self._zipped_xml_file_path = os.path.join(
-            self._xml_folder_path, "Gesamtdatenexport_%s.zip" % self._today_string
         )
         self._sqlite_folder_path = os.path.join(
             expanduser("~"), ".open-MaStR", "data", "sqlite"
@@ -52,14 +39,15 @@ class Mastr:
 
     def download(
         self,
-        method=None,
+        method="bulk",
+        bulk_date_string="today",
         bulk_include_tables=None,
         bulk_exclude_tables=None,
         cleansing=True,
         processes=None,
         technology=None,
         limit=None,
-        date=None,
+        api_date=None,
         chunksize=None,
         data_types=None,
         location_types=None,
@@ -91,20 +79,35 @@ class Mastr:
 
         """
         if method == "bulk":
+
+            # Find the name of the zipped xml folder
+            if bulk_date_string == "today":
+                bulk_date_folder_extension = date.today().strftime("%Y%m%d")
+            else:
+                try:
+                    bulk_date_folder_extension = isoparse(bulk_date_string).strftime("%Y%m%d")
+                except dateutil.parser.ParserError:
+                    print("date_string has to be a proper date in the format yyyymmdd.")
+                    raise
+
+            _zipped_xml_file_path = os.path.join(
+                self._xml_folder_path, f"Gesamtdatenexport_{bulk_date_folder_extension}.zip"
+            )
+
             if bulk_include_tables and bulk_exclude_tables:
                 raise Exception(
                     "Either include_tables or exclude_tables has to be None."
                 )
 
-            if os.path.exists(self._zipped_xml_file_path):
+            if os.path.exists(_zipped_xml_file_path):
                 print("MaStR already downloaded.")
             else:
                 shutil.rmtree(self._xml_folder_path, ignore_errors=True)
                 os.makedirs(self._xml_folder_path, exist_ok=True)
                 print("MaStR is downloaded to %s" % self._xml_folder_path)
-                download_xml_Mastr(self._zipped_xml_file_path)
+                download_xml_Mastr(_zipped_xml_file_path)
 
-            with ZipFile(self._zipped_xml_file_path, "r") as f:
+            with ZipFile(_zipped_xml_file_path, "r") as f:
                 full_list_of_files = [
                     entry.split(".")[0].split("_")[0].lower() for entry in f.namelist()
                 ]
@@ -121,7 +124,7 @@ class Mastr:
 
             convert_mastr_xml_to_sqlite(
                 con=self._bulk_sql_connection,
-                zipped_xml_file_path=self._zipped_xml_file_path,
+                zipped_xml_file_path=_zipped_xml_file_path,
                 include_tables=bulk_include_tables,
             )
             if cleansing:
@@ -132,7 +135,11 @@ class Mastr:
 
         if method == "API":
             print(
-                f"Downloading with soap_API.\n\n   -- Settings --  \nunits after date: {date}\nunit donwnload limit per technology: {limit}\nparallel_processes: {processes}\nchunksize: {chunksize}\ntechnologies: {technology}\ndata_types: {data_types}\nlocation_types: {location_types}"
+                f"Downloading with soap_API.\n\n   -- Settings --  \nunits after date: "
+                f"{api_date}\nunit donwnload limit per technology: "
+                f"{limit}\nparallel_processes: {processes}\nchunksize: "
+                f"{chunksize}\ntechnologies: {technology}\ndata_types: "
+                f"{data_types}\nlocation_types: {location_types}"
             )
             mastr_mirror = MaStRMirror(
                 empty_schema=False,
@@ -141,7 +148,7 @@ class Mastr:
                 restore_dump=None,
             )
             # # Download basic unit data
-            # mastr_mirror.backfill_basic(technology, limit=limit, date=date)
+            # mastr_mirror.backfill_basic(technology, limit=limit, date=api_date)
             #
             # # Download additional unit data
             # for tech in technology:
