46b20ca7dabb5d6367740710a5c682495ff1ea1d
Florian Kotthoff <florian.kotthoff@posteo.de>
2022-02-08 14:46:17 +0100

    Rename Database path variable

--- open_mastr/settings.py
+++ open_mastr/settings.py
@@ -4,12 +4,12 @@ from os.path import expanduser
 # TODO: Migrate the DB settings from the bulk download
 DB_ENGINE = os.environ.get("DB_ENGINE", "sqlite")
 
-sqlite_database_path = os.path.join(
+SQLITE_DATABASE_PATH = os.environ.get("SQLITE_DATABASE_PATH", os.path.join(
             expanduser("~"), ".open-MaStR", "data", "sqlite", "open-mastr.db"
-        )
+        ))
 
 print(DB_ENGINE)
 DB_URL = (
     "postgresql+psycopg2://open-mastr:open-mastr@localhost:55443/open-mastr"
-    if DB_ENGINE == "docker" else f"sqlite:///{sqlite_database_path}"
+    if DB_ENGINE == "docker" else f"sqlite:///{SQLITE_DATABASE_PATH}"
 )
