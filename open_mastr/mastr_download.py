79c6ced3b2576c783202ba7e64d719ab1dc62f2a
cwm <djeenie@posteo.de>
2022-03-04 14:28:54 +0000

    Format with black

--- open_mastr/mastr_download.py
+++ open_mastr/mastr_download.py
@@ -5,14 +5,30 @@ from open_mastr.settings import DB_ENGINE
 mastr = Mastr()
 
 # configure settings for API download
-date='latest' # TODO: initiate_string -> maybe centralize function
-limit = None
+date = "latest"  # TODO: initiate_string -> maybe centralize function
+limit = 1
 processes = 12
 chunksize = 1000
-technology = ["wind", "biomass", "combustion", "gsgk", "hydro", "nuclear", "storage", "solar"]
-data_types = ["unit_data", "eeg_data", "kwk_data", "permit_data"]
-location_types = ["location_elec_generation", "location_elec_consumption", "location_gas_generation",
-                  "location_gas_consumption"]
+technology = [
+    "wind"
+]  # , "biomass", "combustion", "gsgk", "hydro", "nuclear", "storage", "solar"]
+data_types = ["unit_data", "eeg_data", "kwk_data", "permit_data"]  #
+location_types = [
+    "location_elec_generation",
+    "location_elec_consumption",
+    "location_gas_generation",
+    "location_gas_consumption",
+]
 
 # call download functionality
-mastr.download(method='API', processes=processes, limit=limit, chunksize=chunksize, technology=technology, date=date, data_types=data_types, location_types=location_types, initialize_db=DB_ENGINE)
+mastr.download(
+    method="API",
+    processes=processes,
+    limit=limit,
+    chunksize=chunksize,
+    technology=technology,
+    api_date=date,
+    data_types=data_types,
+    location_types=location_types,
+    initialize_db=DB_ENGINE,
+)
