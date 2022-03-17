36ead23b14481e072b8a6376234a3c89b42a7b1b
Deniz Tepe <deniztepe92@gmail.com>
2022-03-17 12:52:36 +0100

    Initial commit for ci test

---
+++ open_mastr/dummy_ci.py
@@ -0,0 +1,2 @@
+def dummy_ci_add(a, b):
+    return a+b
---
+++ tests/test_ci.py
@@ -0,0 +1,9 @@
+from open_mastr.dummy_ci import dummy_ci_add
+
+
+def test_ci_1():
+    assert 6 == dummy_ci_add(4, 2)
+
+
+def test_ci_2():
+    assert 'Hello world' == dummy_ci_add('Hello', 'world')
