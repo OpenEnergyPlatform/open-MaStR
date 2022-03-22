7e9b16cd62400495e1e38f45169628176c54d366
Florian Kotthoff <florian.kotthoff@posteo.de>
2022-03-22 12:30:01 +0100

    Create test for parameter validity check
    In the two new test functions, both working and none working parameters are checked.
    
    [Ticket: #207]

--- tests/test_mastr.py
+++ tests/test_mastr.py
@@ -1,8 +1,10 @@
-from datetime import date
+from datetime import datetime
 from open_mastr.mastr import Mastr
 import os
 import sqlalchemy
 import sqlite3
+import pytest
+
 
 def test_Mastr_init():
     db = Mastr()
@@ -15,16 +17,82 @@ def test_Mastr_init():
     assert type(db._sql_connection) == sqlite3.Connection
 
 
-#def test_Mastr_download():
-#    date_string = "20211206"
-#    mastr = Mastr(date_string=date_string)
-#    assert mastr._today_string == date_string
+def test_Mastr_validate_working_parameter():
+    db = Mastr()
+    parameter_dict_working = {
+        "method": ["API", "bulk"],
+        "technology": [
+            "wind",
+            "solar",
+            "biomass",
+            "hydro",
+            "gsgk",
+            "combustion",
+            "nuclear",
+            "gas",
+            "storage",
+            "electricity_consumer",
+            "location",
+            "market",
+            "grid",
+            "balancing_area",
+            "permit",
+        ],
+        "bulk_date_string": ["today", "20200108"],
+        "bulk_cleansing": [True, False],
+        "api_processes": [2, 20, None, "max"],
+        "api_limit": [15, None],
+        "api_date": [None, datetime(2022, 2, 2), "latest"],
+        "api_chunksize": [20],
+        "api_data_types": ["unit_data", "eeg_data", "kwk_data", "permit_data"],
+        "api_location_types": [
+            "location_elec_generation",
+            "location_elec_consumption",
+            "location_gas_generation",
+            "location_gas_consumption",
+        ],
+    }
+    parameter_dict = {}
+    for key in list(parameter_dict_working.keys()):
+        parameter_dict[key] = parameter_dict_working[key][0]
 
-    #mastr = Mastr()
-    #assert len(mastr._today_string) == 8
+    # working parameters
+    for key in list(parameter_dict_working.keys()):
+        for value in parameter_dict_working[key]:
+            parameter_dict[key] = value
+            assert (
+                db._validate_parameter_format_for_download_method(parameter_dict)
+                is None
+            )
 
-    #include_tables=["einheitenwind"]
-    #mastr.download(method="bulk", include_tables=include_tables)
-    #assert os.path.getsize(mastr._zipped_xml_file_path) > 900000000
 
+def test_Mastr_validate_not_working_parameter():
+    db = Mastr()
+    parameter_dict_not_working = {
+        "method": [5, "BULK", "api"],
+        "technology": [
+            "wint",
+            "Solar",
+            "biomasse",
+            5,
+            None,
+        ],
+        "bulk_date_string": ["heute", "20. April 1999", 20202202],
+        "bulk_cleansing": ["cleansing", 4, None],
+        "api_processes": ["20", "None"],
+        "api_limit": ["15", "None"],
+        "api_date": ["None", "20220202"],
+        "api_chunksize": [None, "20"],
+        "api_data_types": ["unite_data", 5, None],
+        "api_location_types": ["locatione_elec_generation", 5, None],
+    }
+    parameter_dict = {}
+    for key in list(parameter_dict_not_working.keys()):
+        parameter_dict[key] = parameter_dict_not_working[key][0]
 
+    # working parameters
+    for key in list(parameter_dict_not_working.keys()):
+        for value in parameter_dict_not_working[key]:
+            parameter_dict[key] = value
+            with pytest.raises(Exception):
+                db._validate_parameter_format_for_download_method(parameter_dict)
