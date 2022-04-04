9c1d071832918bc6f564d464a2711db8010b8aea
Florian Kotthoff <florian.kotthoff@posteo.de>
2022-04-04 09:49:12 +0200

    Delete print command
    The print command was executed when importing the package and is now
    deleted.

--- open_mastr/settings.py
+++ open_mastr/settings.py
@@ -8,7 +8,6 @@ SQLITE_DATABASE_PATH = os.environ.get("SQLITE_DATABASE_PATH", os.path.join(
             expanduser("~"), ".open-MaStR", "data", "sqlite", "open-mastr.db"
         ))
 
-print(DB_ENGINE)
 DB_URL = (
     "postgresql+psycopg2://open-mastr:open-mastr@localhost:55443/open-mastr"
     if DB_ENGINE == "docker" else f"sqlite:///{SQLITE_DATABASE_PATH}"
