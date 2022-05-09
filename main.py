6a018b82be0aeb2f7e91b1ebfb18098a0bb9308a
Ludee <Ludwig.Huelk@rl-institut.de>
2022-05-09 13:25:55 +0200

    Add main.py to execute download

---
+++ main.py
@@ -0,0 +1,55 @@
+from open_mastr import Mastr
+
+from open_mastr.postprocessing.cleaning import cleaned_data
+from postprocessing.postprocessing import postprocess
+from postprocessing.postprocessing import to_csv
+
+import datetime
+
+db = Mastr()
+
+# Download
+technology = ["wind",
+              "biomass",
+              "combustion",
+              "gsgk",
+              "hydro",
+              "nuclear",
+              "storage"]
+
+api_data_types = [
+    "unit_data",
+    "eeg_data",
+    "kwk_data",
+    "permit_data"
+]
+
+api_location_types = [
+    "location_elec_generation",
+    "location_elec_consumption",
+    "location_gas_generation",
+    "location_gas_consumption",
+]
+
+# db.download(method="bulk",
+#             api_date='latest',
+#             api_limit=10,
+#             technology=technology,
+#             api_data_types=api_data_types,
+#             api_location_types=api_location_types)
+
+# db.download(method="API",
+#             api_date='latest',
+#             api_limit=10,
+#             technology=technology,
+#             api_data_types=api_data_types,
+#             api_location_types=api_location_types)
+
+# db.to_csv()
+
+# Postprocessing
+
+cleaned = cleaned_data()
+postprocess(cleaned)
+
+to_csv()
