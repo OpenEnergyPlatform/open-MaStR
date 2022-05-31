64cc7411849051acee48817c4eb71af470dc200d
Ludee <Ludwig.Huelk@rl-institut.de>
2022-05-31 14:40:20 +0200

    Improve main file with metadata and more technologies #236

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+open-MaStR - Main file
+
+Bulk: Download XML-Dump and fill in local SQLite database.
+API: Download latest entries using the SOAP-API.
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+"""
+
 from open_mastr import Mastr
 
 # from open_mastr.postprocessing.cleaning import cleaned_data
@@ -7,14 +19,32 @@ from open_mastr import Mastr
 db = Mastr()
 
 ## Download
-technology = ["wind",
-              "biomass",
-              "combustion",
-              "gsgk",
-              "hydro",
-              "nuclear",
-              "storage"]
-# excluded: "solar"
+technology_bulk = ["biomass",
+                   "combustion",
+                   "gas",
+                   "gsgk",
+                   "hydro",
+                   "nuclear",
+                   "solar",
+                   "storage",
+                   "wind",
+
+                   "balancing_area",
+                   "electricity_consumer",
+                   "grid",
+                   "location",
+                   "market",
+                   "permit"
+                   ]
+
+technology_api = ["biomass",
+                  "combustion",
+                  "gas",
+                  "gsgk",
+                  "hydro",
+                  "nuclear",
+                  "storage",
+                  "wind", ]
 
 api_data_types = [
     "unit_data",
@@ -32,14 +62,18 @@ api_location_types = [
 
 if __name__ == "__main__":
 
+    # Bulk download
     db.download(method="bulk",
-                technology=technology,
+                technology=technology_bulk,
                 api_data_types=api_data_types)
 
+    # API download
     db.download(method="API",
                 api_date='latest',
+                api_chunksize=1000,
                 api_limit=10,
-                technology=technology,
+                api_processes='max',
+                technology=technology_api,
                 api_data_types=api_data_types,
                 api_location_types=api_location_types)
 
