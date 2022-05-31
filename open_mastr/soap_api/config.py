3753ad8a76cce67047c36a087a7ebb24cfad4092
cwm <djeenie@posteo.de>
2022-05-31 18:10:12 +0100

    Fix naming of location tables Close #261
    For this change to take effect, manually delete /.open-MaStR/config/filename.yml. and run main.py

--- open_mastr/soap_api/config.py
+++ open_mastr/soap_api/config.py
@@ -209,7 +209,7 @@ def _filenames_generator():
         # Add filenames for location data
         location_types = ["location_elec_generation", "location_elec_consumption", "location_gas_generation",
             "location_gas_consumption"]
-        filenames["raw"].update({loc: f"{loc}_raw.csv" for loc in location_types})
+        filenames["raw"].update({loc: f"{prefix}_{loc}_raw.csv" for loc in location_types})
 
         # Add metadata file
         filenames["metadata"] = "datapackage.json"
