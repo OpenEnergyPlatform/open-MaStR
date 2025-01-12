5803ed163749141d00db52746995a31e14d1fce0
AlexandraImbrisca <alexandra.imbrisca6@gmail.com>
2025-01-12 18:13:43 +0100

    Add unit tests for all of the new and existing methods

--- open_mastr/xml_download/utils_write_to_database.py
+++ open_mastr/xml_download/utils_write_to_database.py
@@ -6,7 +6,7 @@ import lxml
 import numpy as np
 import pandas as pd
 import sqlalchemy
-from sqlalchemy import select, inspect
+from sqlalchemy import inspect
 from sqlalchemy.sql import text
 from sqlalchemy.sql.sqltypes import Date, DateTime
 
