4be6399b693b819edd5049277a1c8af86f53ffe8
FlorianK13 <florian.kotthoff@posteo.de>
2025-10-27 14:03:29 +0100

    Revert changes for delayed testing strategy #622
    This is now handled in the workflow .yaml files

--- tests/test_mastr.py
+++ tests/test_mastr.py
@@ -2,11 +2,8 @@ from open_mastr.mastr import Mastr
 import os
 import sqlalchemy
 import pytest
-import requests
 import pandas as pd
 from open_mastr.utils.constants import TRANSLATIONS
-import random
-import time
 
 
 @pytest.fixture
@@ -21,24 +18,8 @@ def db():
     path = os.path.join(
         os.path.expanduser("~"), ".open-MaStR", "data", "sqlite", "mastr-test.db"
     )
-    engine = sqlalchemy.create_engine(f"sqlite:///{path}")
-    db = Mastr(engine=engine)
-
-    NUMBER_DOWNLOAD_TRIES_FROM_MASTR = 10
-    for i in range(NUMBER_DOWNLOAD_TRIES_FROM_MASTR):
-        # marktstammdatenregister seems to break the connection if too many download requests are send
-        # from the same IP. Within github CLI we generate 9 parallel requests, hence the random sleep
-        # between each try.
-        sleep_time = random.uniform(0, 8)
-        time.sleep(sleep_time)
-
-        try:
-            db.download(data="electricity_consumer")
-            print("MaStR Downloaded!")
-            break
-        except requests.exceptions.ConnectionError:
-            continue
-    db.engine.dispose()
+    db = Mastr(engine=sqlalchemy.create_engine(f"sqlite:///{path}"))
+    db.download(data="electricity_consumer")
     return db
 
 
@@ -51,20 +32,18 @@ def test_Mastr_init(db):
     assert type(db.engine) == sqlalchemy.engine.Engine
 
 
-# def test_Mastr_translate(db, db_path):
-#     db.translate()
-#     # test if database was renamed correctly
-#     transl_path = db_path[:-3] + "-translated.db"
-#     assert os.path.exists(transl_path)
-#
-#     # test if columns got translated
-#     inspector = sqlalchemy.inspect(db.engine)
-#     table_names = inspector.get_table_names()
-#
-#     for table in table_names:
-#         for column in inspector.get_columns(table):
-#             column = column["name"]
-#             assert column in TRANSLATIONS.values() or column not in TRANSLATIONS.keys()
+def test_Mastr_translate(db, db_path):
+    db.translate()
+    # test if database was renamed correctly
+    transl_path = db_path[:-3] + "-translated.db"
+    assert os.path.exists(transl_path)
+    # test if columns got translated
+    inspector = sqlalchemy.inspect(db.engine)
+    table_names = inspector.get_table_names()
+    for table in table_names:
+        for column in inspector.get_columns(table):
+            column = column["name"]
+            assert column in TRANSLATIONS.values() or column not in TRANSLATIONS.keys()
 
 
 def test_mastr_download(db):
